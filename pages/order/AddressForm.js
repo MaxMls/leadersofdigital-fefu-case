b1584326354b1fb709228f993fd332a4ccee3e35
Alexander Mikhailenko <molotok539@gmail.com>
2020-10-02 22:04:51 +1000

    Add order page.

---
+++ pages/order/AddressForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function AddressForm() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Адресс доставки
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="firstName"
+            name="firstName"
+            label="Имя"
+            fullWidth
+            autoComplete="given-name"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Фамилия"
+            fullWidth
+            autoComplete="family-name"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="address1"
+            name="address1"
+            label="Адресс"
+            fullWidth
+            autoComplete="shipping address-line1"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+            label="Использовать этот адресс для платёжных реквизитов"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
